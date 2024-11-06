50a5340296d6e3d955bda225b6ff85053200bccd
ArenasFD <diego.arenas@edu.sait.ca>
2024-11-06 16:46:16 -0700

    Lab 2

---
+++ lab.js
@@ -0,0 +1,10 @@
+elk.addEventListener ("click", function(){
+pagetitle.innerHtml= "Elk";
+console.log ("elk");
+document.body.classList.remove("black bear");
+document.body.classList.remove ("bighorn sheep");
+document.body.classList.remove ("wolf");
+document.getElementById ("banner"). src="elk.webp";
+document. getElementById (banner)
+}
+)

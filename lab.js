484450b4541a16355faf37874f1c874f21184aef
ArenasFD <diego.arenas@edu.sait.ca>
2024-11-06 17:06:05 -0700

    JS

--- lab.js
+++ lab.js
@@ -4,7 +4,20 @@ console.log ("elk");
 document.body.classList.remove("black bear");
 document.body.classList.remove ("bighorn sheep");
 document.body.classList.remove ("wolf");
+document.body.classList.add()
 document.getElementById ("banner"). src="elk.webp";
-document. getElementById (banner)
+document. getElementById ("banner"). src=
 }
 )
+
+elk.addEventListener ("click", function(){
+    pagetitle.innerHtml= "Elk";
+    console.log ("elk");
+    document.body.classList.remove("black bear");
+    document.body.classList.remove ("bighorn sheep");
+    document.body.classList.remove ("wolf");
+    document.body.classList.add()
+    document.getElementById ("banner"). src="elk.webp";
+    document. getElementById ("banner"). src=
+    }
+    )
